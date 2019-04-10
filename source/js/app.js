b7a161d77083edb8baca4d8c801cc95bc82197b6
Denis <denisvovchenko96@mail.ru>
2019-04-10 07:48:52 +0200

    small fixes

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,8 @@
 let navToggle = document.querySelector('.js-nav__toggle');
 let navList = document.querySelector('.js-nav__list');
 
-navToggle.addEventListener('click', e => {
-  e.preventDefault();
+navToggle.addEventListener('click', (evt) => {
+  evt.preventDefault();
 
   navList.classList.toggle('nav__list--show');
   navToggle.classList.toggle('nav__toggle--close');
