c8ff70ffc73541c37edb3c8d5cd7ee04819f9beb
Denys <denisvovchenko96@mail.ru>
2019-05-27 23:19:44 +0200

    tablet nav

--- source/js/app.js
+++ source/js/app.js
@@ -1,16 +1,19 @@
 let body = document.querySelector('.page');
 let navToggle = document.querySelector('.js-nav__toggle');
-let navList = document.querySelector('.js-nav__list');
+let navLists = document.querySelectorAll('.js-nav__list');
 
 navToggle.addEventListener('click', (evt) => {
   evt.preventDefault();
 
-  navList.classList.toggle('nav__list--show');
+  navLists.forEach(function(list) {
+    list.classList.toggle('nav__list--show');
+  });
+
   navToggle.classList.toggle('nav__toggle--close');
 });
 
 if ( body.classList.contains('page--index') ) {
-  let indexLink = document.querySelector('.page-header__logo');
+  let indexLink = document.querySelector('.logo');
 
   indexLink.removeAttribute('href', '');
 }
